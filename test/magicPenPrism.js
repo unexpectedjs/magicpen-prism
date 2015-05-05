10d1e831c10c4879e5eebab9afb25c41e0de47e7
Andreas Lind <andreas@one.com>
2015-05-05 22:41:30 +0200

    Highlight XML.

--- test/magicPenPrism.js
+++ test/magicPenPrism.js
@@ -24,6 +24,25 @@ describe('magicpen-prism', function () {
         );
     });
 
+    it('should highlight a language containing "+xml" as markup', function () {
+        expect(
+            magicPen.code(
+                '<?xml version="1.0"?>\n' +
+                '<svg width="120" height="120" viewBox="0 0 120 120"\n' +
+                '     xmlns="http://www.w3.org/2000/svg" version="1.1"\n' +
+                '     xmlns:xlink="http://www.w3.org/1999/xlink">\n' +
+                '    <circle cx="60" cy="60" r="50" />\n' +
+                '</svg>',
+                'image/svg+xml').toString('ansi'),
+            'to equal',
+                '\u001b[90m\u001b[38;5;110m<?xml version="1.0"?>\u001b[39m\n' +
+                '\u001b[90m\u001b[38;5;247m<\u001b[39m\u001b[34m\u001b[38;5;162msvg \u001b[39m\u001b[32m\u001b[38;5;112mwidth\u001b[39m\u001b[90m\u001b[38;5;247m="\u001b[39m\u001b[90m\u001b[38;5;74m120\u001b[39m\u001b[90m\u001b[38;5;247m"\u001b[39m\u001b[34m\u001b[38;5;162m \u001b[39m\u001b[32m\u001b[38;5;112mheight\u001b[39m\u001b[90m\u001b[38;5;247m="\u001b[39m\u001b[90m\u001b[38;5;74m120\u001b[39m\u001b[90m\u001b[38;5;247m"\u001b[39m\u001b[34m\u001b[38;5;162m \u001b[39m\u001b[32m\u001b[38;5;112mviewBox\u001b[39m\u001b[90m\u001b[38;5;247m="\u001b[39m\u001b[90m\u001b[38;5;74m0 0 120 120\u001b[39m\u001b[90m\u001b[38;5;247m"\u001b[39m\n' +
+                '\u001b[34m\u001b[38;5;162m     \u001b[39m\u001b[32m\u001b[38;5;112mxmlns\u001b[39m\u001b[90m\u001b[38;5;247m="\u001b[39m\u001b[90m\u001b[38;5;74mhttp://www.w3.org/2000/svg\u001b[39m\u001b[90m\u001b[38;5;247m"\u001b[39m\u001b[34m\u001b[38;5;162m \u001b[39m\u001b[32m\u001b[38;5;112mversion\u001b[39m\u001b[90m\u001b[38;5;247m="\u001b[39m\u001b[90m\u001b[38;5;74m1.1\u001b[39m\u001b[90m\u001b[38;5;247m"\u001b[39m\n' +
+                '\u001b[34m\u001b[38;5;162m     \u001b[39mxmlns:\u001b[32m\u001b[38;5;112mxlink\u001b[39m\u001b[90m\u001b[38;5;247m="\u001b[39m\u001b[90m\u001b[38;5;74mhttp://www.w3.org/1999/xlink\u001b[39m\u001b[90m\u001b[38;5;247m">\u001b[39m\n' +
+                '    \u001b[90m\u001b[38;5;247m<\u001b[39m\u001b[34m\u001b[38;5;162mcircle \u001b[39m\u001b[32m\u001b[38;5;112mcx\u001b[39m\u001b[90m\u001b[38;5;247m="\u001b[39m\u001b[90m\u001b[38;5;74m60\u001b[39m\u001b[90m\u001b[38;5;247m"\u001b[39m\u001b[34m\u001b[38;5;162m \u001b[39m\u001b[32m\u001b[38;5;112mcy\u001b[39m\u001b[90m\u001b[38;5;247m="\u001b[39m\u001b[90m\u001b[38;5;74m60\u001b[39m\u001b[90m\u001b[38;5;247m"\u001b[39m\u001b[34m\u001b[38;5;162m \u001b[39m\u001b[32m\u001b[38;5;112mr\u001b[39m\u001b[90m\u001b[38;5;247m="\u001b[39m\u001b[90m\u001b[38;5;74m50\u001b[39m\u001b[90m\u001b[38;5;247m"\u001b[39m\u001b[34m\u001b[38;5;162m \u001b[39m\u001b[90m\u001b[38;5;247m/>\u001b[39m\n\u001b[90m\u001b[38;5;247m</\u001b[39m\u001b[34m\u001b[38;5;162msvg\u001b[39m\u001b[90m\u001b[38;5;247m>\u001b[39m'
+        );
+    });
+
     it('should not touch an unsupported language', function () {
         expect(
             magicPen.code('var foo = "bar";', 'foobar').toString('ansi'),
